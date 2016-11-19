215edb086aefb61a7cb4dc0689f518c1c67e1731
Nejc <kezzy.13@gmail.com>
2016-11-19 14:02:51 +0100

    Initial commit

---
+++ app/src/main/java/com/example/nejc/interactivestory/ui/StoryActivity.java
@@ -0,0 +1,103 @@
+package com.example.nejc.interactivestory.ui;
+
+import android.content.Intent;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
+import android.support.v4.content.res.ResourcesCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.nejc.interactivestory.R;
+import com.example.nejc.interactivestory.model.Page;
+import com.example.nejc.interactivestory.model.Story;
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.appindexing.Thing;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+public class StoryActivity extends AppCompatActivity {
+
+    public static final String TAG = StoryActivity.class.getSimpleName();
+
+    private Story mStory = new Story();
+    private ImageView mImageView;
+    private TextView mTextView;
+    private Button mChoice1;
+    private Button mChoice2;
+    private String mName;
+    private Page mCurrentPage;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient mClient;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_story);
+
+        Intent intent = getIntent();
+        mName = intent.getStringExtra("name");
+
+        if (mName == null) {
+            mName = "Friend";
+        }
+        Log.d(TAG, mName);
+
+        mImageView = (ImageView) findViewById(R.id.storyImageView);
+        mTextView = (TextView) findViewById(R.id.storyTextView);
+        mChoice1 = (Button) findViewById(R.id.choiceButton1);
+        mChoice2 = (Button) findViewById(R.id.choiceButton2);
+
+        loadPage(0);
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        mClient = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
+    }
+
+    private void loadPage(int choice) {
+        mCurrentPage = mStory.getPage(choice);
+
+        Drawable drawable = ResourcesCompat.getDrawable(getResources(), mCurrentPage.getImageId(), null);
+        mImageView.setImageDrawable(drawable);
+
+        String pageText = mCurrentPage.getText();
+        // Add the name if placeholder included. Won't add if no placeholder
+        pageText = String.format(pageText, mName);
+        mTextView.setText(pageText);
+
+        if(mCurrentPage.isFinal()){
+            mChoice1.setVisibility(View.INVISIBLE);
+            mChoice2.setText("PLAY AGAIN");
+            mChoice2.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View v) {
+                    finish();
+                }
+            });
+        }
+        else {
+            mChoice1.setText(mCurrentPage.getChoice1().getText());
+            mChoice2.setText(mCurrentPage.getChoice2().getText());
+
+            mChoice1.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View v) {
+                    int nextPage = mCurrentPage.getChoice1().getNextPage();
+                    loadPage(nextPage);
+                }
+            });
+
+            mChoice2.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View v) {
+                    int nextPage = mCurrentPage.getChoice2().getNextPage();
+                    loadPage(nextPage);
+                }
+            });
+        }
+    }
+}
